4472c70cbda6cf112bf711c4049475e7de110cdc
hamza.ayoub <hamza.ayoub@systemsltd.com>
2023-09-13 13:38:59 +0500

    add-button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p className="text-black text-md">
-        Hello world
-      </p>
+      <button
+        onClick={() => {
+          router.push("./resturant/kfc");
+        }}
+        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+      >
+        Click To View
+      </button>
     </main>
   );
 }
